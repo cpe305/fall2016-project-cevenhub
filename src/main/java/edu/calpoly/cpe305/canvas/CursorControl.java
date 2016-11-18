10fb633539ef68518422eaa142133008805312fe
cevenhub <cevenmail@gmail.com>
2016-11-18 13:23:54 -0800

    Added Line Drawing Functionality
    Can now draw lines onto the canvas. Have not yet added functionality
    for changing line attributes and to limit drawing area to canvas.

--- src/main/java/edu/calpoly/cpe305/canvas/CursorControl.java
+++ src/main/java/edu/calpoly/cpe305/canvas/CursorControl.java
@@ -3,4 +3,5 @@ package edu.calpoly.cpe305.canvas;
 // Controller portion of cursor, for implementing drawing functionality
 public class CursorControl {
 
+
 }
--- src/main/java/edu/calpoly/cpe305/canvas/StartCanvas.java
+++ src/main/java/edu/calpoly/cpe305/canvas/StartCanvas.java
@@ -1,12 +1,21 @@
 package edu.calpoly.cpe305.canvas;
 
+import java.util.ArrayList;
+
 import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Line;
 import javafx.scene.control.Slider;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
@@ -31,6 +40,7 @@ public class StartCanvas extends Application {
     Pane rootNode = new Pane();
     StackPane stack = new StackPane();
     Canvas toolBackground = new Canvas(WIDTH / 8, HEIGHT);
+    final ArrayList<Line> lineList = new ArrayList<Line>();
 
     stack.setStyle(GRAY);
     stack.getChildren().add(toolBackground);
@@ -39,16 +49,39 @@ public class StartCanvas extends Application {
     VBox radioButtons = CanvasToolModel.radioButtons();
     VBox penEraser = CanvasToolModel.penOrEraser();
     VBox rgbSlider = CanvasToolModel.rgbSliders();
-    
+    final Group lineNodes = new Group();
+
     rootNode.getChildren().add(stack);
     rootNode.getChildren().add(sizeSlider);
     rootNode.getChildren().add(radioButtons);
     rootNode.getChildren().add(penEraser);
     rootNode.getChildren().add(rgbSlider);
+    rootNode.getChildren().add(lineNodes);
 
     Scene scene = new Scene(rootNode, WIDTH, HEIGHT);
+
+    scene.setOnMousePressed(new EventHandler<MouseEvent>() {
+      public void handle(MouseEvent event) {
+        Line line = new Line();
+        line.setStartX(event.getX());
+        line.setStartY(event.getY());
+        lineList.add(line);
+        System.out.println("Start X: " + line.getStartX() + " Start Y: " + line.getStartY());
+      }
+    });
+    scene.setOnMouseReleased(new EventHandler<MouseEvent>() {
+      public void handle(MouseEvent event) {
+        lineList.get(lineList.size() - 1).setEndX(event.getX());
+        lineList.get(lineList.size() - 1).setEndY(event.getY());
+        System.out.println("End X: " + lineList.get(lineList.size() - 1).getEndX() + " End Y: "
+            + lineList.get(lineList.size() - 1).getEndY());
+        lineNodes.getChildren().add(lineList.get(lineList.size() - 1));
+      }
+    });
+
     window.setScene(scene);
     window.show();
+
   }
 
   public void createToolBar() {
